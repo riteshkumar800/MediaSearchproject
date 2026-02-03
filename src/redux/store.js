b2d0fe0ee6cf81b87829b4d829d1f7910954e75b
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-02-04 01:57:41 +0530

    add

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import searchReducer from './features/searchSlice'
+import collectionSlice from './features/collectionSlice'
+
+
+
+export const store=configureStore({
+    reducer:{
+        
+    }
+})
