4556e89f3ca9f09df2b8efe45d5c8955327dd282
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-02-05 23:52:06 +0530

    added

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -18,7 +18,10 @@ const tabs=()=>{
                     <button
                         className={`${(activeTab==elem?'bg-blue-700':'bg-gray-500')} transition cursor-pointer active:scale-95 px-5 py-2 rounded uppercase`}
                         key={idx}
-                    ></button>
+                        onClick={()=>{
+                            dispatch(setActiveTabs(elem))
+                        }}
+                    > {elem}</button>
 
                 )
 
